0e5e5b1efa2df8d6fd9b11e0d8f7456a70f794b2
albertlin <albert5718@gmail.com>
2025-04-28 21:10:33 +0000

    feat(bin): Add obmc binaries installation
    - Install tmux, minicom, vim
    - Install checkin, checkin_output_banner script
    - Install default minicom settings
    - Install default tmux settings

---
+++ stage2/04-install-obmc-binaries/01-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Ensure the home directory exists for the default user (e.g., pi)
+USER_HOME="${ROOTFS_DIR}/home/pi"
+
+# Copy scripts to /usr/bin
+install -m 755 files/checkin "${ROOTFS_DIR}/usr/bin/"
+install -m 755 files/checkin_output_banner "${ROOTFS_DIR}/usr/local/bin/"
+
+# Copy the custom .tmux.conf file
+install -v -o 1000 -g 1000 -m 644 files/tmux/.tmux.conf "${USER_HOME}/.tmux.conf"
+
+# Ensure the minicom configuration directory exists
+install -d "${ROOTFS_DIR}/etc/minicom"
+# Copy the custom minicom configuration
+install -m 644 files/minicom/minirc.bmc "${ROOTFS_DIR}/etc/minicom/minirc.bmc"
+install -m 644 files/minicom/minirc.dfl "${ROOTFS_DIR}/etc/minicom/minirc.dfl"
+
