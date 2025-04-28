9b5417b447aea5318d8bfbc58ca40273ecd5181d
albertlin <albert5718@gmail.com>
2025-04-28 21:10:51 +0000

    feat(config): add PDU power control and BIOS console support
    Add power control capability and BIOS console access:
    - Add new 'ac' command for PDU power control
    - Install ipmitool package for IPMI operations
    - Add minicom configuration for BIOS serial console
    
    The 'ac' command provides automated PDU control based on hostname:
    - Auto-maps Pi numbers (01-98) to corresponding PDU outlets
    - Supports power on/off/cycle operations
    - Includes safety checks and status verification
    - Example: 'ac cycle --times 3 --period 5'
    
    This change enables remote power control without physical access and
    adds BIOS console access capability for debugging.

--- stage2/04-install-obmc-binaries/01-run.sh
+++ stage2/04-install-obmc-binaries/01-run.sh
@@ -6,6 +6,7 @@ USER_HOME="${ROOTFS_DIR}/home/pi"
 
 # Copy scripts to /usr/bin
 install -m 755 files/checkin "${ROOTFS_DIR}/usr/bin/"
+install -m 755 files/ac "${ROOTFS_DIR}/usr/bin/"
 install -m 755 files/checkin_output_banner "${ROOTFS_DIR}/usr/local/bin/"
 
 # Copy the custom .tmux.conf file
@@ -15,5 +16,6 @@ install -v -o 1000 -g 1000 -m 644 files/tmux/.tmux.conf "${USER_HOME}/.tmux.conf
 install -d "${ROOTFS_DIR}/etc/minicom"
 # Copy the custom minicom configuration
 install -m 644 files/minicom/minirc.bmc "${ROOTFS_DIR}/etc/minicom/minirc.bmc"
+install -m 644 files/minicom/minirc.bios "${ROOTFS_DIR}/etc/minicom/minirc.bios"
 install -m 644 files/minicom/minirc.dfl "${ROOTFS_DIR}/etc/minicom/minirc.dfl"
 
